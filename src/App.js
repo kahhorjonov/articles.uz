edb8f5253f36cd25c9360deb285493d414022fca
kahhorjonov <akbarjon3028@gmail.com>
2022-02-22 19:41:07 +0500

    123

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ import ReviewerLayout from "./layouts/Reviewer.js";
 import UserLayout from "./layouts/User.js";
 import { ToastContainer } from "react-toastify";
 import HomePage from "./components/UI/homePage";
-import Asosiy from "./components/UI/asosiy";
-import JurnallarRoyxati from "components/UI/jurnallarRo'yhati";
-import NashrShartlari from "components/UI/nashrShartlari";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -54,20 +51,6 @@ class App extends Component {
             component={ReviewerRegisterForm}
           />
 
-          <Route path="/" exact component={(props) => <Asosiy {...props} />} />
-
-          <Route
-            path="/listOfMagazines"
-            exact
-            component={(props) => <JurnallarRoyxati {...props} />}
-          />
-
-          <Route
-            path="/termsOfPublication"
-            exact
-            component={(props) => <NashrShartlari {...props} />}
-          />
-
           {/* <Route path="/not-found" component={NotFound} /> */}
           {/* <Redirect from="/admin" to="/admin/dashboard" /> */}
           {/* <ProtectedRoute path="/admin" component={AdminPage} /> */}
@@ -75,11 +58,12 @@ class App extends Component {
           {/* <ProtectedRoute path="/reviewer" component={ReviewerPage} /> */}
           {/* <ProtectedRoute path="/user" component={UserPage} /> */}
 
-          <Redirect from="/admin" exact to="/admin/dashboard" />
+          {/* <Redirect from="/admin" exact to="/admin/dashboard" />
           <Redirect from="/reductor" exact to="/reductor/myTasks" />
           <Redirect from="/reviewer" exact to="/reviewer/myTasks" />
-          <Redirect from="/user" exact to="/user/user-page" />
-          {/* <Redirect to="/not-found" /> */}
+          <Redirect from="/user" exact to="/user/user-page" /> */}
+
+          <Redirect to="/not-found" />
 
           <ProtectedRoute
             path="/admin"
--- src/components/UI/homePage.jsx
+++ src/components/UI/homePage.jsx
@@ -6,41 +6,15 @@ import JurnallarRoyxati from "./jurnallarRo'yhati";
 import NashrShartlari from "components/UI/nashrShartlari";
 
 import "../../styles/homePage.css";
-import LoginForm from "./../loginForm";
 
 class HomePage extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <>
-          <NavbarHome />
-          <Switch>
-            <Route
-              path="/"
-              exact
-              component={(props) => <Asosiy {...props} />}
-            />
-
-            <Route
-              path="/listOfMagazines"
-              exact
-              component={(props) => <JurnallarRoyxati {...props} />}
-            />
-
-            <Route
-              path="/termsOfPublication"
-              exact
-              component={(props) => <NashrShartlari {...props} />}
-            />
-
-            <Route
-              path="/termsOfPublication"
-              exact
-              component={(props) => <NashrShartlari {...props} />}
-            />
-          </Switch>
-        </>
-      </BrowserRouter>
+      <>
+        <BrowserRouter>
+          <Asosiy />
+        </BrowserRouter>
+      </>
     );
   }
 }
