e83ef29e1efeed37f1b8afb7caa67c2a67ec2312
xojaaa <96516074+xojaaa@users.noreply.github.com>
2022-02-24 15:19:29 +0300

    123

--- src/components/UI/jurnallarRo'yhati.jsx
+++ src/components/UI/jurnallarRo'yhati.jsx
@@ -218,80 +218,13 @@ class JurnallarRoyxati extends Component {
                 <hr />
               </div>
 
-              <div className="col-md-9">
-                <div
-                  id="carouselExampleIndicators"
-                  className="carousel slide"
-                  data-bs-ride="carousel"
-                >
-                  <div className="carousel-indicators">
-                    <button
-                      type="button"
-                      data-bs-target="#carouselExampleIndicators"
-                      data-bs-slide-to="0"
-                      className="active"
-                      aria-current="true"
-                      aria-label="Slide 1"
-                    ></button>
-                    <button
-                      type="button"
-                      data-bs-target="#carouselExampleIndicators"
-                      data-bs-slide-to="1"
-                      aria-label="Slide 2"
-                    ></button>
-                    <button
-                      type="button"
-                      data-bs-target="#carouselExampleIndicators"
-                      data-bs-slide-to="2"
-                      aria-label="Slide 3"
-                    ></button>
-                  </div>
-                  <div className="carousel-inner">
-                    <div className="carousel-item active">
-                      <div className="card">
-                        <img
-                          className="card-img-top"
-                          src={img}
-                          alt="Card image"
-                        />
-                        <div className="card-body">
-                          <h4 className="card-title">John Doe</h4>
-                          <p className="card-text">Some example text.</p>
-                          <a href="#" className="btn btn-primary">
-                            See Profile
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="carousel-item"></div>
-                    <div className="carousel-item"></div>
-                  </div>
-
-                  <button
-                    className="carousel-control-prev"
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide="prev"
-                  >
-                    <span
-                      className="carousel-control-prev-icon"
-                      aria-hidden="true"
-                    ></span>
-                    <span className="visually-hidden">Previous</span>
-                  </button>
-                  <button
-                    className="carousel-control-next"
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide="next"
-                  >
-                    <span
-                      className="carousel-control-next-icon"
-                      aria-hidden="true"
-                    ></span>
-                    <span className="visually-hidden">Next</span>
-                  </button>
-                </div>
+              <div className="col-md-4">
+                <ScrollingCarousel>
+                  
+                  <p>Lorem.</p>
+                  <p>Lorem.</p>
+                  <p>Lorem.</p>
+                  </ScrollingCarousel>,
               </div>
             </div>
           </div>
