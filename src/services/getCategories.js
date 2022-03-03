d29fc22d3ad1783996def5878bd0915329934d51
xojaaa <96516074+xojaaa@users.noreply.github.com>
2022-03-03 16:37:48 +0300

    category services page created

--- src/services/getCategories.js
+++ src/services/getCategories.js
@@ -5,5 +5,23 @@ const { apiSwagger, apiLocal } = api;
 
 export function getCategories() {
   return http.get(apiLocal + "/category/all");
-  // return http.get(apiSwagger + "/category/all");
 }
+
+export function changeActivityOfCategory(id) {
+  return http.get(apiLocal + `/category/changeActive/${id}`);
+}
+
+export function deleteCategories(id) {
+  return http.get(apiLocal + `/category/delete/${id}`);
+}
+
+export function createOrEditCategories(data) {
+  return http.post(apiLocal + `/category/saveOrEdit`, data);
+}
+
+export default {
+  getCategories,
+  changeActivityOfCategory,
+  deleteCategories,
+  createOrEditCategories,
+};
