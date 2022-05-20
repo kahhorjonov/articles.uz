410c143b4e6a1152326317601bfaf5daad0a173c
kahhorjonov <akbarjon3028@gmail.com>
2022-05-20 11:29:45 +0500

    browser router changed to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -9,8 +9,8 @@ import "./assets/demo/demo.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
